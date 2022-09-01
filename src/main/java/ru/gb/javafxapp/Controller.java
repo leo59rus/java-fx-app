06f8fa9ca00e4034ca7fe6444157883c23338fc9
leonid <orangr59usa@gmail.com>
2022-09-01 14:48:33 +0300

    продолженеи игры быки коровы

--- src/main/java/ru/gb/javafxapp/Controller.java
+++ src/main/java/ru/gb/javafxapp/Controller.java
@@ -1,14 +1,24 @@
 package ru.gb.javafxapp;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class Controller {
+    @FXML//когда делаем приватными сверху ставим форму
+    private TextArea historyArea;
     @FXML
-    private Label welcomeText;
+    private TextField userAnswer;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Game game;
+
+    public Controller() {
+        this.game = new Game();
+    }
+
+    public void clickCheckButton(ActionEvent actionEvent) {
+
     }
 }
